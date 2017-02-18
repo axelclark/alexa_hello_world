baf6bea729de12117cef1732bf80f9d4dd0ac229
Axel Clark <axelclark2@yahoo.com>
2017-02-17 19:48:34 -0500

    Init New Project

---
+++ lib/alexa_hello_world.ex
@@ -0,0 +1,18 @@
+defmodule AlexaHelloWorld do
+  @moduledoc """
+  Documentation for AlexaHelloWorld.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AlexaHelloWorld.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/alexa_hello_world_test.exs
@@ -0,0 +1,8 @@
+defmodule AlexaHelloWorldTest do
+  use ExUnit.Case
+  doctest AlexaHelloWorld
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
